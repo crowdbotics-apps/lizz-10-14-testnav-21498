3d8267758ef8c0a0be759282b760c4e18e74524b
Crowdbotics <team@crowdbotics.com>
2020-10-14 19:14:59 +0000

    Update components

--- src/features/BlankScreen1142785/screens/blank.js
+++ src/features/BlankScreen1142785/screens/blank.js
@@ -27,9 +27,28 @@ export class _Blank extends React.Component {
   render = () => (
     <View style={styles.View_1}>
       <Text style={styles.Text_4}>This is blank screen 1</Text>
-      <Text style={styles.Text_6}>
-        When this text is pressed, go to blank screen 0
-      </Text>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen0142784")}
+      >
+        <Text style={styles.Text_6}>
+          When this text is pressed, go to blank screen 0
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.View_10}>
+        <View style={styles.View_11}>
+          <Button
+            title="Press me! (does nothing)"
+            color="#3366FF"
+            style={styles.Button_17}
+            onPress={() => alert("Pressed!")}
+          />
+        </View>
+        <View style={styles.View_12}>
+          <Text style={styles.Text_15}>
+            In the visual editor this long text becomes a row
+          </Text>
+        </View>
+      </View>
     </View>
   )
 }
